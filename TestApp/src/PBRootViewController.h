c6a7b21b4637e46e7c9dc7a3d18797f03713a3c1
Pepe Becker <mail@pepebecker.com>
2017-01-17 23:33:59 +0100

    Move source of TestApp to master branch

--- TestApp/src/PBRootViewController.h
+++
@@ -1,3 +0,0 @@
-@interface PBRootViewController : UITableViewController
-
-@end
--- TestApp/src/main.m
+++
@@ -1,7 +0,0 @@
-#import "PBAppDelegate.h"
-
-int main(int argc, char *argv[]) {
-	@autoreleasepool {
-		return UIApplicationMain(argc, argv, nil, NSStringFromClass(PBAppDelegate.class));
-	}
-}
